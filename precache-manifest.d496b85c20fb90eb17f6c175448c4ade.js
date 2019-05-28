c230a2487e5f4f6e3d0de9b96d5b9c4136aa9454
Wojciech Puzio <wojciech.puzio@polcode.net>
2019-05-28 13:51:11 +0200

    Updates

---
+++ precache-manifest.d496b85c20fb90eb17f6c175448c4ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed7b830ee2035e86b5aa3942c89be33",
+    "url": "/poke/index.html"
+  },
+  {
+    "revision": "1e60aa6080ce831fd9ca",
+    "url": "/poke/static/css/main.b52f6eeb.chunk.css"
+  },
+  {
+    "revision": "0579c19214664632bc80",
+    "url": "/poke/static/js/2.4ae1c4df.chunk.js"
+  },
+  {
+    "revision": "1e60aa6080ce831fd9ca",
+    "url": "/poke/static/js/main.5ee18ecc.chunk.js"
+  },
+  {
+    "revision": "a6c984170ba4cdc2a6c2",
+    "url": "/poke/static/js/runtime~main.a8fd8cc3.js"
+  },
+  {
+    "revision": "f5f206080ef03f6b72a8eda7a2949618",
+    "url": "/poke/static/media/background.f5f20608.jpg"
+  }
+]);
